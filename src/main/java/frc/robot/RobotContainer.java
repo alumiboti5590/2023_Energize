d9f03ce4d4ba62d56d2afa89bdad05897b3de05d
Dan Starner <me@danstarner.com>
2023-03-03 21:48:55 -0500

    remove the arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import frc.robot.controllers.IDriverController;
 import frc.robot.controllers.IOperatorController;
 import frc.robot.controllers.XboxDriverController;
 import frc.robot.controllers.XboxOperatorController;
-import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shoulder;
@@ -31,7 +30,6 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private Drivetrain drivetrain;
-  private Arm arm;
   private Intake intake;
   private Shoulder shoulder;
 
